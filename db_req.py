db497f7bd1acc150c413503a2db599d9c6b558b6
Papasha <Pavelevikin@yandex.ru>
2022-06-25 02:57:36 +0300

    ВСЯКИЙ

--- db_req.py
+++ db_req.py
@@ -4,7 +4,7 @@ from loguru import logger
 def select_user_id(user_id):
     query = """SELECT * from user_id where user_id = %s"""
     try:
-        conn = MySQLConnection(host="localhost", user="root", db="screener")
+        conn = MySQLConnection(host="localhost", user="root", db="screener_v2_0")
         cursor = conn.cursor()
         cursor.execute(query, (user_id,))
         result = cursor.fetchall()
@@ -20,7 +20,7 @@ def select_user_id(user_id):
 def select_all_user_id():
     query = """SELECT user_id from user_id """
     try:
-        conn = MySQLConnection(host="localhost", user="root", db="screener")
+        conn = MySQLConnection(host="localhost", user="root", db="screener_v2_0")
         cursor = conn.cursor()
         cursor.execute(query)
         result = cursor.fetchall()
@@ -39,7 +39,7 @@ def insert_user_id(user_id):
         (user_id)
         VALUES (%s);"""
     try:
-        conn = MySQLConnection(host="localhost", user="root", db="screener")
+        conn = MySQLConnection(host="localhost", user="root", db="screener_v2_0")
         cursor = conn.cursor()
         cursor.execute(query, (user_id,))
         conn.commit()
