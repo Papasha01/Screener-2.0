d9c2b70228446d3e16012c8b95b9470464364441
Papasha <Pavelevikin@yandex.ru>
2022-06-24 18:45:42 +0300

    в процессе

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from db_req import select_user_id, select_all_user_id, insert_user_id
 from datetime import date, datetime, timedelta
 from binance.spot import Spot as Client
 import unicorn_binance_websocket_api
@@ -103,7 +104,7 @@ def get_depth_from_websocket():
                     if len(data_price.loc[filter_agg]) == 0:
                         data_price = pd.DataFrame([{'coin': jsMessage['data']['s'], 'price': jsMessage['data']['p']}])
 
-                        print(data_price.loc[(data_price.coin == 'ADAUSDT')].values[0][1])
+                        # print(data_price.loc[(data_price.coin == 'ADAUSDT')].values[0][1])
         else: 
             time.sleep(0.5)
 
@@ -114,28 +115,73 @@ def check_old_data():
     '''
     while True:
         for index, row in data_depth.iterrows():
-            #try:
-                sign_ptd = float(data_price.loc[(data_price.coin == row['coin'])].values[0][1]) / float(row['price'])
-                percentage_to_density = abs((sign_ptd) - 1)
-                if  percentage_to_density <= cf_distance:
+            # try:
+                filter_depth = ((data_depth.coin == row['coin']) & (data_depth.price ==  row['price']))
+                percentage_to_density = abs((float(data_price.loc[(data_price.coin == row['coin'])].values[0][1]) / float(row['price'])) - 1)
+                if  percentage_to_density <= cf_distance and (float(row['quantity']) * float(row['price'])) > limit:
                     if datetime.now() - time_resend > row['dt_resend'] and row['in_range'] != 1:
 
-                        filter_depth = ((data_depth.coin == row['coin']) & (data_depth.price == row['price']))
                         data_depth.dt_resend.where(~(filter_depth), other=datetime.now(), inplace=True) 
                         data_depth.in_range.where(~(filter_depth), other=1, inplace=True) 
-                        
-                        if sign_ptd > 1: percentage_to_density = -percentage_to_density
-                        #print(f'\n\nCoin: { row['coin'] }\nPrice: {record[2]}\nQuantity: {record[3]}\nAmount: {round(float(record[2]) * float(record[3]), 2)}$\nPercentage to density: {round(percentage_to_density*100, 2)}%\nDate of discovery: {record[4]}')
-                        #send_telegram(record, percentage_to_density)
-                        #logger.debug(f'{str(record)} {str(percentage_to_density)})')
-                        # sound_notification.play()
-                #else: 
-                    #update_out_from_range(record[1], record[2])
-            #except Exception as e:
-                #logger.error(e)
 
-                # time.sleep(10)
-                check_old_data()
+                        print(f"\n\nCoin: {row['coin']}\nPrice: {row['price']}\nQuantity: {row['quantity']}\nAmount: {round(float(row['quantity']) * float(row['price']), 2)}$\nPercentage to density: {round(percentage_to_density*100, 2)}%\nDate of discovery: {row['dt']}")
+                        send_telegram(row, percentage_to_density)
+                        logger.debug(f'{str(row)} {str(percentage_to_density)})')
+
+                else: 
+                    data_depth.in_range.where(~(filter_depth), other=0, inplace=True) 
+            # except Exception as e:
+            #     logger.error(e)
+            #     time.sleep(10)
+            #     check_old_data()
+
+token = '5276441681:AAHi9DX8ZYWVlm49AEBU1be0gVEXWmeKoZ8'
+bot=telebot.TeleBot(token)
+
+# Запуск цикла Telebot
+def polling():
+    time.sleep(5)
+    try: 
+        bot.polling(none_stop=True) 
+    except Exception as e: 
+        logger.error(e)
+        time.sleep(5)
+        polling()
+
+# Взаимодействие с ботом
+@bot.message_handler(commands=['start'])
+def start_handler(message):
+    bot.send_message(message.chat.id, "I'm working!")
+    logger.debug(f"User №{message.chat.id} send I'm working)")
+    if not select_user_id(str(message.chat.id)):
+        insert_user_id(str(message.chat.id))
+        logger.debug(f'User №{message.chat.id} added to the database)')
+
+@bot.message_handler(commands=['check'])
+def start_handler(message):
+    filter_depth = ((data_depth.in_range ==  1))
+    if len(data_depth.loc[filter_depth]) == 0:
+        logger.debug(f"User №{message.chat.id} send No records)")
+        bot.send_message(message.chat.id, 'No records')
+    else:
+        for index, row in data_depth.iterrows():
+            if row['in_range'] == 1:
+                percentage_to_density = abs((float(data_price.loc[(data_price.coin == row)].values[0][1]) / float(row['price'])) - 1)
+                all_verified_message += f"Coin: {row['coin']}\nPrice: {row['price']}\nQuantity: {row['quantity']}\nAmount: {round(float(row['price']) * float(row['quantity']), 2)}$\nPercentage to density: {round(percentage_to_density*100, 2)}%\nDate of discovery: {row['dt']}\n\n"
+                logger.debug(f"User №{message.chat.id} send all_verified_message")
+                bot.send_message(message.chat.id, 'sss')
+        
+
+# Отправка уведомления в телеграм
+def send_telegram(row, percentage_to_density):
+    if select_all_user_id():
+        for user_id in select_all_user_id():
+            try:
+                bot.send_message(user_id[0], f"Coin: {row['coin']}\nPrice: {row['price']}\nQuantity: {row['quantity']}\nAmount: {round(float(row['price']) * float(row['quantity']), 2)}$\nPercentage to density: {round(percentage_to_density*100, 2)}%\nDate of discovery: {row['dt']}\n\n")
+            except telebot.apihelper.ApiException as e:
+                if e.description == "Forbidden: bot was blocked by the user":
+                    print(f"Attention please! The user {user_id[0]} has blocked the bot")
+
 
 
 # Global variable
@@ -143,18 +189,24 @@ delta, time_resend, limit, cf_update, cf_distance = import_cfg()
 data_depth = pd.DataFrame(columns=['coin', 'price', 'quantity', 'dt', 'dt_resend', 'in_range'])
 data_price = pd.DataFrame(columns=['coin', 'price'])
 list_coin = get_list_coins()
+spinner_running = Spinner('Checking ')
+
 ubwa = unicorn_binance_websocket_api.BinanceWebSocketApiManager(exchange="binance.com")
 ubwa.create_stream(['depth', 'aggTrade'], list_coin)
 print('Successful connection')
 
-def main():
-    # Progress
-    spinner_running = Spinner('Checking ')
+# Значек работы программы
+def spin():
+    while True:
+        time.sleep(0.2)
+        spinner_running.next()
 
-    data_depth = get_first_data(list_coin=list_coin)
+def main():
+    get_first_data(list_coin=list_coin)
     Thread(target=get_depth_from_websocket).start()
     Thread(target=check_old_data).start()
-    #get_depth_from_websocket(list_coin= list_coin)
+    Thread(target=polling).start()
+    spin()
 
 if __name__ == '__main__':
     main()
---
+++ db_req.py
@@ -0,0 +1,50 @@
+from mysql.connector import MySQLConnection, Error
+from loguru import logger
+
+def select_user_id(user_id):
+    query = """SELECT * from user_id where user_id = %s"""
+    try:
+        conn = MySQLConnection(host="localhost", user="root", db="screener")
+        cursor = conn.cursor()
+        cursor.execute(query, (user_id,))
+        result = cursor.fetchall()
+        if len(result) > 0:
+            return(result[0])
+        else:
+            return(False)
+    except Error as error:
+        logger.error(error)
+    finally:
+        conn.close()
+
+def select_all_user_id():
+    query = """SELECT user_id from user_id """
+    try:
+        conn = MySQLConnection(host="localhost", user="root", db="screener")
+        cursor = conn.cursor()
+        cursor.execute(query)
+        result = cursor.fetchall()
+        if len(result) > 0:
+            return(result)
+        else:
+            return(False)
+    except Error as error:
+        logger.error(error)
+    finally:
+        conn.close()
+
+
+def insert_user_id(user_id):
+    query = """INSERT INTO user_id
+        (user_id)
+        VALUES (%s);"""
+    try:
+        conn = MySQLConnection(host="localhost", user="root", db="screener")
+        cursor = conn.cursor()
+        cursor.execute(query, (user_id,))
+        conn.commit()
+    except Error as error:
+        logger.error(error)
+    finally:
+        conn.close()    
+
